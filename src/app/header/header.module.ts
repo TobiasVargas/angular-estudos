c187e5c0704dbac587ea8792bb082db39acb03e8
Tobias de Vargas <42094615+TobiasVargas@users.noreply.github.com>
2020-07-04 11:26:52 -0300

    commit initial

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class HeaderModule { }
